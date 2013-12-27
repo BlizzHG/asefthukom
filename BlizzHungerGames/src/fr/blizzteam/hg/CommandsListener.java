c296c81e5b66e9afddc8641989dce22688adcc4a
TEprogamers <TEprogamers@live.fr>
2013-12-27 22:26:03 +0100

    dddddd

--- BlizzHungerGames/src/fr/blizzteam/hg/CommandsListener.java
+++ BlizzHungerGames/src/fr/blizzteam/hg/CommandsListener.java
@@ -1,22 +1,26 @@
 package fr.blizzteam.hg;
 
 import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 
 public class CommandsListener implements Listener {
+	
+	HungerGames hgmain;
         
-        private HungerGames plugin;
-        public CommandsListener(HungerGames plugin) {
-                this.plugin = plugin;
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("help")) {
+                sender.sendMessage(ChatColor.GREEN + "command help");
+                return true;
         }
-        
-        @EventHandler
-        public void onPlayerMove(PlayerMoveEvent e) {
-                Player p = e.getPlayer();
-                p.sendMessage(ChatColor.GREEN + "You have moved!");
-                        
-                }
+        if (cmd.getName().equalsIgnoreCase("who")) {
+            sender.sendMessage(ChatColor.GREEN + "command who");
+            return true;
         }
+        return true;
+    }
+}
