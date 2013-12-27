ddfa999e13894fe69b533c63f8843060c6a1cebd
TEprogamers <TEprogamers@live.fr>
2013-12-27 22:04:03 +0100

    Cadeau

--- BlizzHungerGames/src/fr/blizzteam/hg/HungerGames.java
+++ BlizzHungerGames/src/fr/blizzteam/hg/HungerGames.java
@@ -5,30 +5,19 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
  
-public class HungerGames extends JavaPlugin {
+public class HungerGames extends JavaPlugin implements Listener {
  
-        public void onEnable() {
-                Bukkit.getServer().getLogger().info("Test Plugin Enabled!");
-        }
-       
-        public void onDisable() {
-                Bukkit.getServer().getLogger().info("Test Plugin Disabled!");
-        }
-       
-        public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
-               
-                if (!(sender instanceof Player)) {
-                        sender.sendMessage(ChatColor.AQUA + "The console ran the test command! Good job!");
-                        return true;
-                }
-               
-                Player player = (Player) sender;
-               
-                if (cmd.getName().equalsIgnoreCase("test")) {
-                        player.sendMessage(ChatColor.GOLD + "You ran the test command! Good job!");
-                }
-                return true;
-        }
+    public void onEnable() {
+        getConfig().options().copyDefaults(true);
+        saveConfig();
+        Bukkit.getServer().getPluginManager().registerEvents(this, this);
+}
+    public void onStart() {
+    	
+    }
+    
+    
 }
---
+++ BlizzHungerGames/src/fr/blizzteam/hg/managers/HungerGamesManager.java
@@ -0,0 +1,5 @@
+package fr.blizzteam.hg.managers;
+
+public class HungerGamesManager {
+
+}
