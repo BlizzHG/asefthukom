403afcc4cc14f40267702a5bdca094f392c036e8
coco_gigpn <coco_gigpn@hotmail.com>
2013-12-28 13:25:11 +0100

    sd

--- BlizzHungerGames/src/Commands/List.java
+++ BlizzHungerGames/src/Commands/List.java
@@ -1,5 +1,8 @@
 package Commands;
 
+import java.util.ArrayList;
+
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -11,20 +14,28 @@ import fr.blizzteam.hg.HungerGames;
 public class List implements CommandExecutor { 
 	
 	public HungerGames plugin;
-	 
+	public static ArrayList<String> players = new ArrayList();
     public List(HungerGames instance) {
     	
             plugin = instance;
     }
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-            if (cmd.getName().equalsIgnoreCase("list") || cmd.getName().equalsIgnoreCase("who"))  {
+            if (cmd.getName().equalsIgnoreCase("list"))  {
                     if (!(sender instanceof Player)) {
                             sender.sendMessage(ChatColor.DARK_RED + "Erreur: Le joueur n'est pas reconnue!");
                             return true;
                     }
                     Player player = (Player) sender;
-                    player.sendMessage("§4Groupes§7: §4Admins§2 ,§cTechnicien ,§5Moderateur ,§6Pro ,§1Mvp ,§aVip ,§7Default");
+                    String admin = "§4";
+                    String technicien = "§c";
+                    String moderateur = "§5";
+                    String pro = "§6";
+                    String mvp = "§1";
+                    String vip = "§a";
+                    String defaut = "§7";
+                    player.sendMessage("§4Groupes§7: §4Admins§2 ,§cTechnicien§2 ,§5Moderateur§2 ,§6Pro§2 ,§1Mvp§2 ,§aVip§2 ,§7Default");
+                    
             return true;
     }
 			return false;
